ee6d2a51f4e2073cfe7cf905237e93652c8a35ba
SERIN Kévin <kserin@toulouse.viveris.com>
2020-09-09 09:54:33 +0200

    Unify usage of double quotes in webpack config

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "src/index.html",
@@ -9,7 +9,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
 });
 
 const cssExtractPlugin = new MiniCssExtractPlugin({
-  filename: '[name].css'
+  filename: "[name].css"
 });
 
 const cleanPlugin = new CleanWebpackPlugin();
